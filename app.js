508f695188ca213b1c0b59d8abee5ba9926b3f65
James Sibaja <jsibajagranados2@gmail.com>
2024-09-24 15:36:25 -0600

    horario de riego arreglo

--- app.js
+++ app.js
@@ -3,23 +3,10 @@ const blynkToken = '3gO4NB3Cj7xA3V35tTvwauapvX0N4D76';
 const apiUrl = `https://ny3.blynk.cloud/external/api/get?token=${blynkToken}`;
 
 // Función para obtener el horario actual desde Blynk
-function getSchedule() {
-    fetch(apiUrl + '&v0&v1&v2&v3')
-        .then(response => response.json())
-        .then(data => {
-            // Actualiza la interfaz con los valores actuales
-            document.getElementById('schedule').innerHTML = `
-                <p>Tiempo de espera 1: ${data.v0} minutos</p>
-                <p>Tiempo de riego 1: ${data.v1} minutos</p>
-                <p>Tiempo de espera 2: ${data.v2} minutos</p>
-                <p>Tiempo de riego 2: ${data.v3} minutos</p>
-            `;
-        })
-        .catch(error => console.error('Error al obtener el horario:', error));
-}
+
 
 function getSchedule() {
-    fetch(apiUrl + '&v0&v1&v2&v3&v4&v5&v6&v7')
+    fetch(apiUrl + '&v0&v1&v2&v4&v5&v6&v7&v8&v9&v10&v11&v12')
         .then(response => response.json())
         .then(data => {
             let scheduleHtml = '';
