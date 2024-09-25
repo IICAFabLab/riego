d6bd61706b58237d2caeb12b9d6d159ddaf56793
JamesSibaja <jsibajagranados2@gmail.com>
2024-09-24 22:19:22 -0600

    información bateia

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const apiUrl = `https://ny3.blynk.cloud/external/api/get?token=${blynkToken}`;
 
 
 function getSchedule() {
-    fetch(apiUrl + '&v0&v1&v2&v4&v5&v6&v7&v8&v9&v10&v11&v12')
+    fetch(apiUrl + '&v0&v1&v2&v4&v5&v6&v7&v8&v9&v10&v11&v12v15')
         .then(response => response.json())
         .then(data => {
             let scheduleHtml = '';
@@ -88,6 +88,8 @@ function getSchedule() {
                 `;
                 pairCount++;
             }
+            let bat = `${data.v15}`;
+            document.getElementById('bateria').innerHTML = bat;
             if (pairCount > 1) {
                 document.getElementById('schedule').innerHTML = scheduleHtml ;
             }else{
