bf8d35c4fcab9ce8ea5b80c4daab7346160c3be2
JamesSibaja <jsibajagranados2@gmail.com>
2024-09-24 21:33:18 -0600

    tabla de riego estilos

--- app.js
+++ app.js
@@ -9,14 +9,14 @@ function getSchedule() {
     fetch(apiUrl + '&v0&v1&v2&v4&v5&v6&v7&v8&v9&v10&v11&v12')
         .then(response => response.json())
         .then(data => {
-            let scheduleHtml = '<table>';
+            let scheduleHtml = '';
             let pairCount = 1;
             let tiempoRiego = 0;
 
             if (data.v0 > 0) {
                 tiempoRiego = data.v1;
                 scheduleHtml += `
-                    <tr class="watering-card-compact">
+                    <tr class="highlight">
                         <td><strong>Riego ${pairCount}</strong></td>                        
                         <td><strong>Hora de inicio:</strong> ${((7 + Math.floor(tiempoRiego / 60)) % 13) + Math.floor((7 + Math.floor(tiempoRiego / 60)) / 13)}:${tiempoRiego % 60}</td>
                         <td><strong>Duración:</strong> ${data.v0} minutos</td>
@@ -29,7 +29,7 @@ function getSchedule() {
             if (data.v2 > 0) {
                 tiempoRiego = data.v0+data.v1+data.v4;
                 scheduleHtml += `
-                    <tr class="watering-card-compact">
+                    <tr class="highlight">
                         <td><strong>Riego ${pairCount}</strong></td>
                         <td><strong>Hora de inicio:</strong> ${((7 + Math.floor(tiempoRiego / 60)) % 13) + Math.floor((7 + Math.floor(tiempoRiego / 60)) / 13)}:${tiempoRiego % 60}</td>
                         <td><strong>Duración:</strong> ${data.v2} minutos</td>
@@ -41,7 +41,7 @@ function getSchedule() {
             if (data.v5 > 0) {
                 tiempoRiego = data.v0+data.v1+data.v4+data.v2+data.v6;
                 scheduleHtml += `
-                    <tr class="watering-card-compact">
+                    <tr class="highlight">
                         <td><strong>Riego ${pairCount}</strong></td>
                         <td><strong>Hora de inicio:</strong> ${((7 + Math.floor(tiempoRiego / 60)) % 13) + Math.floor((7 + Math.floor(tiempoRiego / 60)) / 13)}:${tiempoRiego % 60}</td>
                         <td><strong>Duración:</strong> ${data.v5} minutos</td>
@@ -52,7 +52,7 @@ function getSchedule() {
             if (data.v7 > 0) {
                 tiempoRiego = data.v0+data.v1+data.v2+data.v4+data.v5+data.v6+data.v8;
                 scheduleHtml += `
-                    <tr class="watering-card-compact">
+                    <tr class="highlight">
                         <td><strong>Riego ${pairCount}</strong></td>
                         <td><strong>Hora de inicio:</strong> ${((7 + Math.floor(tiempoRiego / 60)) % 13) + Math.floor((7 + Math.floor(tiempoRiego / 60)) / 13)}:${tiempoRiego % 60}</td>
                         <td><strong>Duración:</strong> ${data.v7} minutos</td>
@@ -63,7 +63,7 @@ function getSchedule() {
             if (data.v9 > 0) {
                 tiempoRiego = data.v0+data.v1+data.v2+data.v4+data.v5+data.v6+data.v7+data.v8+data.v10;
                 scheduleHtml += `
-                    <tr class="watering-card-compact">
+                    <tr class="highlight">
                         <td><strong>Riego ${pairCount}</strong></td>
                         <td><strong>Hora de inicio:</strong> ${((7 + Math.floor(tiempoRiego / 60)) % 13) + Math.floor((7 + Math.floor(tiempoRiego / 60)) / 13)}:${tiempoRiego % 60}</td>
                         <td><strong>Duración:</strong> ${data.v9} minutos</td>
@@ -74,7 +74,7 @@ function getSchedule() {
             if (data.v11 > 0) {
                 tiempoRiego = data.v0+data.v1+data.v2+data.v4+data.v5+data.v6+data.v7+data.v8+data.v9+data.v10+data.v12;
                 scheduleHtml += `
-                    <tr class="watering-card-compact">
+                    <tr class="highlight">
                         <td><strong>Riego ${pairCount}</strong></td>
                         <td><strong>Hora de inicio:</strong> ${((7 + Math.floor(tiempoRiego / 60)) % 13) + Math.floor((7 + Math.floor(tiempoRiego / 60)) / 13)}:${tiempoRiego % 60}</td>
                         <td><strong>Duración:</strong> ${data.v11} minutos</td>
@@ -82,7 +82,6 @@ function getSchedule() {
                 `;
                 pairCount++;
             }
-            scheduleHtml += '</table>';
             if (pairCount > 1) {
                 document.getElementById('schedule').innerHTML = scheduleHtml ;
             }else{
